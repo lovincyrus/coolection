8f9dd0d9aa60d5b4f2711233a8f663413791bad8
Cyrus Goh <hello@cyrusgoh.com>
2024-05-08 11:10:15 +0800

    input css tweaks

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -2,17 +2,19 @@
 
 import { SearchIcon } from "lucide-react";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import React, { useCallback, useEffect, useRef } from "react";
+import React, { useRef } from "react";
 import { useHotkeys } from "reakeys";
+import { useDebouncedCallback } from "use-debounce";
 
 import { Footer } from "./footer";
-import { useGlobals } from "./globals-provider";
 import { Header } from "./header";
 import { Results } from "./results";
 
+// After user stops typing for 300ms, update the URL with the new search query
+const DEBOUNCE_TIME = 300;
+
 export function HomePage() {
   const inputRef = useRef<HTMLInputElement>(null);
-  const { toggleSearch } = useGlobals();
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
@@ -28,13 +30,7 @@ export function HomePage() {
     },
   ]);
 
-  useEffect(() => {
-    if (toggleSearch) {
-      inputRef.current?.focus();
-    }
-  }, [toggleSearch]);
-
-  const handleSearch = useCallback(
+  const handleSearch = useDebouncedCallback(
     (event: React.ChangeEvent<HTMLInputElement>) => {
       const params = new URLSearchParams(searchParams);
       if (event.target.value) {
@@ -44,7 +40,7 @@ export function HomePage() {
       }
       replace(`${pathname}?${params.toString()}`);
     },
-    [pathname, replace, searchParams],
+    DEBOUNCE_TIME,
   );
 
   return (
@@ -53,19 +49,17 @@ export function HomePage() {
         <Header />
 
         <div className="mt-20 flex flex-col">
-          {toggleSearch && (
-            <div className="relative">
-              <SearchIcon className="text-muted-foreground absolute left-2 top-2.5 h-4 w-4 opacity-60 grayscale" />
-              <input
-                ref={inputRef}
-                className="focus:shadow-outline w-full appearance-none rounded border border-gray-300 px-3 py-2 pl-8 text-sm leading-tight text-gray-700 transition-colors focus-visible:outline-none focus-visible:ring-1"
-                placeholder="Search..."
-                defaultValue={searchParams.get("q")?.toString()}
-                onChange={handleSearch}
-                autoFocus
-              />
-            </div>
-          )}
+          <div className="relative">
+            <SearchIcon className="text-muted-foreground absolute left-2 top-2 h-4 w-4 opacity-60 grayscale" />
+            <input
+              ref={inputRef}
+              className="focus:shadow-outline w-full appearance-none rounded bg-black/5 px-3 py-2 pl-8 text-sm leading-tight text-gray-700 transition-colors focus-visible:outline-none focus-visible:ring-1"
+              placeholder="Search..."
+              defaultValue={searchParams.get("q")?.toString()}
+              onChange={handleSearch}
+              autoFocus
+            />
+          </div>
 
           <div className="my-8">
             <Results query={searchParams.get("q")?.toString() ?? ""} />
