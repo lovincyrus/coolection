95f66dba946cf42151192e73c1c51cbdcc1dc529
Cyrus Goh <hello@cyrusgoh.com>
2024-05-12 13:25:27 +0800

    fix signout refresh

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -29,7 +29,7 @@ export function Header() {
             onClick={() => {
               signOut(() => {
                 router.push("/");
-                window.location.reload();
+                router.refresh();
               });
             }}
           >
