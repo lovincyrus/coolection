1f1b2c1587d85d7400a81264034544193edf3360
Cyrus Goh <hello@cyrusgoh.com>
2024-05-13 17:43:58 +0800

    max w tweaks

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -7,8 +7,8 @@ export function Footer({ type = "root" }: { type?: "home" | "root" }) {
     <div className="h-[49px] w-full border-t bg-gray-100/60 p-4 text-xs">
       <div
         className={cn(
-          "mx-auto flex w-full max-w-2xl flex-wrap items-center justify-between gap-2 text-gray-500 xl:max-w-4xl",
-          type === "home" ? "2xl:max-w-6xl" : "",
+          "mx-auto flex w-full max-w-2xl flex-wrap items-center justify-between gap-2 text-gray-500",
+          type === "home" ? "xl:max-w-4xl 2xl:max-w-6xl" : "",
         )}
       >
         <div>&#169; {new Date().getFullYear()} Coolection</div>
