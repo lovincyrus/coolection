c277d9885b821d9d7de36128b1b3ba3228cdc953
Cyrus Goh <hello@cyrusgoh.com>
2024-05-14 17:08:36 +0800

    use built in redirect url after signout

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,7 +2,6 @@
 
 import { useClerk } from "@clerk/clerk-react";
 import { LogOutIcon } from "lucide-react";
-import { useRouter } from "next/navigation";
 import React from "react";
 
 import { NewItemDialog } from "./new-item-dialog";
@@ -10,7 +9,6 @@ import { Button } from "./ui/button";
 
 export function Header() {
   const { signOut } = useClerk();
-  const router = useRouter();
 
   return (
     <div className="mx-auto w-full max-w-2xl xl:max-w-4xl 2xl:max-w-6xl">
@@ -26,12 +24,7 @@ export function Header() {
           <NewItemDialog />
           <Button
             className="focus-visible:ring-ring border-input bg-background hover:bg-accent hover:text-accent-foreground ml-auto h-[30px] items-center justify-center whitespace-nowrap rounded-lg border bg-white px-3 text-xs font-medium shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50"
-            onClick={() => {
-              signOut(() => {
-                router.push("/");
-                router.refresh();
-              });
-            }}
+            onClick={() => signOut({ redirectUrl: "/" })}
           >
             <LogOutIcon className="h-4 w-4" />
           </Button>
