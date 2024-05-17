9792a48af33519a83f2b7dcbb5a5f1c7b93410a6
Cyrus Goh <hello@cyrusgoh.com>
2024-05-17 20:14:07 +0800

    sync items to localStorage, perf

--- app/hooks/use-items.tsx
+++ app/hooks/use-items.tsx
@@ -1,16 +1,47 @@
 "use client";
 
+import { useEffect } from "react";
 import useSWR from "swr";
 
-import { fetcher } from "@/lib/fetcher";
+import { fetcher as originalFetcher } from "@/lib/fetcher";
 
 import { CoolectionItem } from "../types";
 
+// Note: If we want to persist all the swr data in `localStorage`, we can use the provider
+// See: https://swr.vercel.app/docs/advanced/cache.en-US#localstorage-based-persistent-cache
 export function useItems() {
+  const localData = localStorage.getItem("/api/items");
+  const fallbackData = localData ? JSON.parse(localData) : [];
+
+  // Persist the data in localStorage
+  const fetcher = async (url: string) => {
+    const data = await originalFetcher<CoolectionItem[]>(url);
+    localStorage.setItem(url, JSON.stringify(data));
+    return data;
+  };
+
   const { data, isLoading, error, mutate } = useSWR<CoolectionItem[]>(
     "/api/items",
     fetcher,
+    {
+      fallbackData: fallbackData,
+    },
   );
+
+  // Before unloading the app, we write back all the data into `localStorage`.
+  useEffect(() => {
+    const handleBeforeUnload = () => {
+      if (data) {
+        localStorage.setItem("/api/items", JSON.stringify(data));
+      }
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, [data]);
 
   return {
     items: data ?? [],
