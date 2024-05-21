a2a704cb768b1c7990f798738cafaac1dc830646
Cyrus Goh <hello@cyrusgoh.com>
2024-05-21 22:41:23 +0800

    comment out text content extraction usage

--- lib/data/add-website.ts
+++ lib/data/add-website.ts
@@ -1,53 +1,52 @@
 import { ItemType } from "@/app/types/coolection";
-import { getArticleContent } from "@/lib/get-article-content";
 import { getMetatags } from "@/lib/get-metatags";
 import prisma from "@/lib/prisma";
 
-import { replaceNewlinesWithSpaces } from "../utils";
-
 export async function addWebsite(url: string, userId: string) {
   const { title, description } = await getMetatags(url);
-  const {
-    title: articleTitle,
-    author,
-    textContent,
-    excerpt,
-    length,
-    siteName,
-  } = await getArticleContent(url);
+
+  // TODO: Uncomment this when the getArticleContent function is implemented
+  // const {
+  //   title: articleTitle,
+  //   author,
+  //   textContent,
+  //   excerpt,
+  //   length,
+  //   siteName,
+  // } = await getArticleContent(url);
 
   // const generatedEmbedding = await generateEmbedding(
   //   (title ?? "") + " " + (description ?? "")
   // );
 
-  const metadata =
-    textContent !== null
-      ? {
-          title: articleTitle,
-          author: author,
-          excerpt: excerpt,
-          length: length,
-          siteName: siteName,
-        }
-      : {};
+  // const metadata =
+  //   textContent !== null
+  //     ? {
+  //         title: articleTitle,
+  //         author: author,
+  //         excerpt: excerpt,
+  //         length: length,
+  //         siteName: siteName,
+  //       }
+  //     : {};
 
   console.log("adding to collection: ", {
     url,
     title,
     description,
-    content: replaceNewlinesWithSpaces(textContent),
-    metadata,
+    // content: replaceNewlinesWithSpaces(textContent),
+    // metadata,
     // embedding: JSON.stringify(generatedEmbedding),
   });
 
   const newCoolection = await prisma.item.create({
     data: {
       url,
-      title: title ?? articleTitle ?? "Untitled",
-      description: description ?? excerpt,
+      title: title ?? "Untitled",
+      description: description,
       type: ItemType._WEBSITE,
-      content: replaceNewlinesWithSpaces(textContent),
-      metadata,
+      // content: replaceNewlinesWithSpaces(textContent),
+      // metadata,
       // See: https://github.com/prisma/prisma/discussions/18220#discussioncomment-5266901
       // embedding: JSON.stringify(embedding),
       userId: userId,
